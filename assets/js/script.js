251a6442dd1d4b0a69814e2da983333821817cdd
khoursaaf <khoursa@gmail.com>
2018-10-29 11:02:52 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+var app = angular.module('myApp', []);
+app.directive('myDirective', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attr, mCtrl) {
+            function myValidation(value) {
+                if (value.indexOf("@") > -1) {
+                    mCtrl.$setValidity('charE', true);
+                } else {
+                    mCtrl.$setValidity('charE', false);
+                }
+                return value;
+            }
+            mCtrl.$parsers.push(myValidation);
+        }
+    };
+});
